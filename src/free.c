84b61b825966b274c87245b3a3fcedb472130045
Ander Amorin sanchez <aamorin-@c3r4s6.42urduliz.com>
2021-11-29 15:28:10 +0100

    29/11/2021 15:28

--- src/free.c
+++ src/free.c
@@ -6,11 +6,7 @@
 /*   By: aamorin- <aamorin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 19:11:42 by aamorin-          #+#    #+#             */
-<<<<<<< HEAD
 /*   Updated: 2021/11/29 14:12:35 by aamorin-         ###   ########.fr       */
-=======
-/*   Updated: 2021/11/26 18:20:36 by migarcia         ###   ########.fr       */
->>>>>>> d9108df023b96570da5b786a16965385692d1406
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +37,6 @@ void	free_processes(t_pipe *pipex)
 		if (pipex->exe[i].c_split)
 			ft_frlloc(pipex->exe[i].c_split);
 	}
-	ft_frlloc_int(pipex->pipes, pipex->procecess_num + 1);
-	free(pipex->exe);
-}
-
-void	free_proc(t_pipe *pipex)
-{
-	free(pipex->pid);
 	ft_frlloc_int(pipex->pipes, pipex->procecess_num + 1);
 	free(pipex->exe);
 }
